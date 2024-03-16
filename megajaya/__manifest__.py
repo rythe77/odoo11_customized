eb5b98928425d09eff799a1526bd9bba6f00a4a5
ryanto <ry.the77@gmail.com>
2024-03-16 22:29:09 +0800

    Made megajaya module

---
+++ megajaya/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "megajaya",
+
+    'summary': """
+        Custom request from megajaya""",
+
+    'description': """
+        List of modified items:
+            -
+    """,
+
+    'author': "Ryanto The",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        #'security/ir.model.access.csv',
+        'reports/master_template.xml',
+        'reports/saleorder_document.xml',
+        'reports/inventory_document.xml',
+    ],
+}
