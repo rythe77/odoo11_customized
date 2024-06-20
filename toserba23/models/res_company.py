79f5008b2b2514c7d010cd1c4e17ff99fa5ede64
ryanto <ry.the77@gmail.com>
2024-06-20 19:43:07 +0800

    Minor document header revision

--- toserba23/models/res_company.py
+++ toserba23/models/res_company.py
@@ -6,4 +6,4 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     #Create new fields
-    contact_person = fields.Text()
+    contact_person = fields.Text(help="Info kontak ini akan tampil pada kop dokumen dan template email")
