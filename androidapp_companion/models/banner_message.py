f6a06d61cce62ed6521c3b72c6d9196a92081939
ryanto <ry.the77@gmail.com>
2025-04-07 08:26:47 +0800

    feat: modify picking document report

--- androidapp_companion/models/banner_message.py
+++ androidapp_companion/models/banner_message.py
@@ -6,5 +6,5 @@ class BannerMessage(models.Model):
     _description = "Banner message to display in android app"
     _order = "message"
 
-    message = fields.Text('Banner Message')
+    message = fields.Char('Banner Message')
     app_companion_id = fields.Many2one('app.companion')
